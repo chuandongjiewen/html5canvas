16267acddbd1c3bcf967c0f2371b228d9c70ceb3
yinchuandong <yincd520@sina.com>
2014-08-06 00:11:50 +0800

    新增框架

---
+++ game.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+	Game.init();
+}
+
+var Game = {
+	init: function(){
+		console.log('init');
+		var canvas = document.getElementById("game");
+		var context = canvas.getContext("2d");
+		//var image = new Image();
+		//image.src = "image/1.png";
+		//if(image.complete){
+		//	context.drawImage(this, 0, 0, this.width, this.height);
+		//}else{
+		//	image.onload = function(){
+		//		context.drawImage(this, 0, 0, this.width, this.height);
+		//	//	context.drawImage(this, 0, 0, 100, 100, 0, 0, 100, 100);
+		//	}
+		//}
+		
+		context.save();	
+		//draw geometric shape
+		// var g1 = context.createRadialGradient(100, 150, 10, 300, 150, 50);
+		//g1.addColorStop(0.1, 'rgb(255, 0, 0)');
+		//g1.addColorStop(0.5, 'rgb(0, 255, 0)');
+		//g1.addColorStop(1, 'rgb(0, 0, 255)');
+		//context.fillStyle = g1;
+		//context.fillRect(0, 0, 400, 300);
+		var g1 = context.createRadialGradient(200, 150, 0, 200, 150, 100);
+        g1.addColorStop(0.1, 'rgb(255,0,0)');  
+        g1.addColorStop(1, 'rgb(50,0,0)');
+        context.fillStyle = g1;
+        context.beginPath();
+        context.arc(200, 150, 100, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
+		
+	}
+};
