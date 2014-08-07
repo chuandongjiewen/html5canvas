0c84d19fa7c13301402eb258c28fbe195a5a7b8b
yinchuandong <yincd520@sina.com>
2014-08-07 10:24:24 +0800

    完成分数的计算

--- game.js
+++ game.js
@@ -15,42 +15,245 @@ function base(derive,baseSprite,baseArgs){
 		proto[prop][SUPER] = baseSprite.prototype;
 	}
 }
+function debug(msg){
+	console.log(msg);
+}
+
+function Floor0(x,y){
+	this.width = 100;
+	this.height = 20;
+	this.x = x;
+	this.y = y;
+}
+
+function Person(x,y){
+	this.width = 40;
+	this.height = 50
+	this.x = x;
+	this.y = y;
+}
+
+
 var Game = {
-	imageArr : {
-		'back':'image/back.png',
-		'floor0' : 'image/floor0.png'
+	width : 320,
+	height : 480,
+	canvas : null,
+	context : null,
+	timer : null, //定时器
+	imageArr : [
+		'image/back.png',
+		'image/floor0.png',
+		'image/floor1.png',
+		'image/floor2.png',
+		'image/floor3.png',
+		'image/person.png'
+	],
+	imageLoadedNum : 0,
+
+	floorList : [],
+	floorVelocity : 0.0005,
+	floorSpeed : 1,
+	floorMaxSpeed : 25,
+
+	person : null,
+	personFloor : null,//人目前站立的floor
+	personFallSpeed : 5,
+	personIsFalling : false,
+	personSumSteps : 4, //按一次方向键走的步数
+	personCurSteps : 0, //当前已经走了多少步
+	curDirection : "none", //保存按键的方向，值为：none, left, right，没有按键则为none
+	preDirection : "none", //保存按键之后，保持的方向
+
+	score : 0, //当前得分
+	isGameOver : false,
+
+	loadImg : function(callback){
+		var self = this;
+		var len = self.imageArr.length;
+		for(var i = 0; i < len; i++){
+			var img = new Image();
+			img.src = self.imageArr[i];
+			img.onload = function(){
+				self.imageLoadedNum ++;
+				if(self.imageLoadedNum == len){
+					callback();
+				}
+			}
+		}
 	},
 	init : function(){
+		var self = this;
 		console.log('init');
-		var canvas = document.getElementById("game");
-		var context = canvas.getContext("2d");
-		//var image = new Image();
-		//image.src = "image/1.png";
-		//if(image.complete){
-		//	context.drawImage(this, 0, 0, this.width, this.height);
-		//}else{
-		//	image.onload = function(){
-		//		context.drawImage(this, 0, 0, this.width, this.height);
-		//	//	context.drawImage(this, 0, 0, 100, 100, 0, 0, 100, 100);
-		//	}
-		//}
+		self.canvas = document.getElementById("game");
+		self.context = self.canvas.getContext("2d");
+		self.initFloor();
+		self.loadImg(function(){
+
+			self.run();
+		});
+	},
+	initFloor : function(){
+		var self = this;
+		var f1 = new Floor0(0, self.height/2);
+		var f2 = new Floor0(self.width/2, self.height);
+		var f3 = new Floor0(200, self.height-200);
+		var f4 = new Floor0(50, self.height-900);
+		self.floorList.push(f1);
+		self.floorList.push(f2);
+		self.floorList.push(f3);
+		self.floorList.push(f4);
+
+		self.person = new Person(30, self.height/2 - 50);
+		self.personFloor = new Floor0(f1.x, f1.y);
+	},
+	drawBack : function(){
+		var self = this;
+		var back = new Image();
+		back.src = "image/back.png";
+		self.context.drawImage(back, 0, 0, self.width, self.height);
+	},
+	drawFloor0 : function(){
+		var self = this;
+		var len = self.floorList.length;
+		for(var i=0; i<len; i++){
+			var floor = self.floorList[i];
+			var floorImg = new Image();
+			floorImg.src = "image/floor0.png";
+			self.context.drawImage(floorImg, floor.x, floor.y, floorImg.width, floorImg.height);
+			floor.y -= self.floorSpeed;
+			if (floor.y < -floor.height) {
+				floor.y = self.height + (Math.random()*10000 % self.height) / 2;
+				floor.x = Math.random()*(self.width - floor.width);
+			}
+		}
 		
-		context.save();	
-		//draw geometric shape
-		// var g1 = context.createRadialGradient(100, 150, 10, 300, 150, 50);
-		//g1.addColorStop(0.1, 'rgb(255, 0, 0)');
-		//g1.addColorStop(0.5, 'rgb(0, 255, 0)');
-		//g1.addColorStop(1, 'rgb(0, 0, 255)');
-		//context.fillStyle = g1;
-		//context.fillRect(0, 0, 400, 300);
-		var g1 = context.createRadialGradient(200, 150, 0, 200, 150, 100);
-        g1.addColorStop(0.1, 'rgb(255,0,0)');  
-        g1.addColorStop(1, 'rgb(50,0,0)');
-        context.fillStyle = g1;
-        context.beginPath();
-        context.arc(200, 150, 100, 0, Math.PI * 2, true);
-        context.closePath();
-        context.fill();
-		
+	},
+	drawPerson : function(){
+		var self = this;
+		var personImg = new Image();
+		personImg.src = "image/person.png";
+		self.context.drawImage(personImg, 0, 0, 40, 50, self.person.x, self.person.y, 40, 50);
+	},
+	drawScore : function(){
+		var self = this;
+		self.context.save();
+		self.context.fillStyle = "#00f";
+		self.context.font = "italic 20px sans-serif";
+		self.context.textBaseline = 'top';             //填充字符串
+		self.context.fillText("score:"+self.score, 0, 10);
+		self.context.restore();
+	},
+	doMove : function(){
+		var self = this;
+		var span = 3;
+		var fallSpan = 2
+		if(self.curDirection == "left"){
+			if(self.personIsFalling == false){
+				if (self.person.x - span >= self.personFloor.x){
+					self.person.x -= span;
+					self.person.y -= self.floorSpeed;
+				}else{
+					self.personIsFalling = true;
+				}
+			}
+			self.preDirection = self.curDirection;
+		}else if(self.curDirection == "right"){
+			if(self.personIsFalling == false){
+				if (self.person.x + span <= self.personFloor.x + self.personFloor.width - self.person.width/2) {
+					self.person.x += span;
+					self.person.y -= self.floorSpeed;
+				}else{
+					self.personIsFalling = true;
+				}
+			}
+			self.preDirection = self.curDirection;
+		}else{
+			self.person.y -= self.floorSpeed;
+		}
+
+		self.curDirection = "none";
+
+		if(!self.personIsFalling){
+			if(self.preDirection != "none" && self.personCurSteps < self.personSumSteps){
+				if(self.preDirection == "left"){
+					self.person.x -= span;
+				}else if(self.preDirection == "right"){
+					self.person.x += span;
+				}
+				self.personCurSteps ++;
+			}else{
+				self.preDirection = "none";
+				self.personCurSteps = 0;
+			}
+		}
+
+		// debug("----"+self.preDirection);
+		if(self.personIsFalling && self.preDirection == "left"){
+			self.person.x = (self.person.x - fallSpan >= 0) ? self.person.x - fallSpan : 0;
+			self.person.y += self.personFallSpeed;
+		}else if(self.personIsFalling && self.preDirection == "right"){
+			self.person.x = (self.person.x + fallSpan <= self.width - self.person.width) ? self.person.x + fallSpan : self.width - self.person.width;
+			self.person.y += self.personFallSpeed;
+		}
+
+		// debug(self.personFloor);	
+	},
+	//检测碰撞
+	detectCollide : function(){
+		var self = this;
+		var len = self.floorList.length;
+		for(var i = 0; i < len; i++){
+			var floor = self.floorList[i];
+			var personBottomX = self.person.x + self.person.width/2;
+			var personBottomY = self.person.y + self.person.height;
+
+			if (personBottomX >= floor.x && personBottomX <= floor.x + floor.width && personBottomY <= floor.y && personBottomY >= floor.y - 5) {
+				if(self.personIsFalling == true){
+					self.score ++;
+					debug("score: " + self.score);
+				}
+				self.personIsFalling = false;
+				self.personFloor = new Floor0(floor.x, floor.y);
+				return;
+			}
+		}
+	},
+	//判断游戏是否结束
+	detectGameOver : function(){
+		var self = this;
+		if((self.person.y + self.person.height/2 <= 0) || (self.person.y >= self.height)){
+			isGameOver = true;
+			clearInterval(self.timer);
+		}
+	},
+	clear : function(){
+		self.context.clearRect(0, 0, self.width, self.height);
+	},
+	run : function(){
+		var self = this;
+
+		window.onkeydown = function(e){
+			var key = e.keyCode;
+			if (key === 80) {  // 'p'
+				// 暂停
+			}if (key === 37) { // left arrow
+				self.curDirection = "left";
+				debug("left");
+			}else if (key === 39) { // right arrow
+				self.curDirection = "right";
+				debug("right");
+			}
+		}
+
+		self.timer = setInterval(function(){
+			self.drawBack();
+			self.drawFloor0();
+			self.drawPerson();
+			self.drawScore();
+			self.doMove();
+			self.detectCollide();
+			self.detectGameOver();
+		}, 1000/60);
 	}
+
 };
